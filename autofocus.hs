0ae338df3f9b7b81ef728cd915fd4fe4dd7faac5
Braden Shepherdson <Braden.Shepherdson@gmail.com>
2010-01-26 10:21:20 -0500

    Lots of work, lost track of the changes.

--- autofocus.hs
+++ autofocus.hs
@@ -51,18 +51,18 @@ flipZip (Zip l x r) = let (x':xs') = reverse l ++ [x] ++ r
 
 commands :: M.Map String Command
 commands = M.fromList [
-             ("add", add)
-            ,("next", next)
-            ,("work", work)
-            ,("done", done)
-            ,("stop", stop)
-            ,("dismiss", dismiss)
-            ,("readd", readd)
-            ,("status", status)
+             ("add", addCmd)
+            ,("next", nextCmd)
+            ,("work", workCmd)
+            ,("done", doneCmd)
+            ,("stop", stopCmd)
+            ,("dismiss", dismissCmd)
+            ,("readd", readdCmd)
+            ,("status", statusCmd)
             ]
 
-add :: Command
-add = do
+addCmd :: Command
+addCmd = do
   putStr "Enter a description of the task: "
   desc <- getLine
   s <- get
@@ -71,34 +71,143 @@ add = do
     Zip l x r -> set $ s { open = Zip l x (r `snoc` desc) }
 
 
+workCmd :: Command
+workCmd = do
+  s <- get
+  case mode s of
+    Work r m -> do io $ putStrLn $ "Currently working on '" ++ r ++ "'."
+                   io $ putStrLn $ "If you are done with it for now, either 'stop' working or mark it 'done'."
+
+    Closed pass acted -> 
+                 case closed s of
+                   Empty -> do io $ putStrLn $ "No task currently selected."
+                               next -- move to the next task
+                               io $ putStrLn $ "You are NOT currently working on this new task. Run 'work' again to work on it, or 'next' to continue searching."
+                   Zip _ x _ -> startWork x (Closed pass (acted+1)) -- start work, and bump the acted-on count for this pass.
+
+    Open -> case open s of
+              Empty -> do io $ putStrLn $ "No task currently selected."
+                          next -- move to the next task
+                          io $ putStrLn $ "You are NOT currently working on this new task. Run 'work' again to work on it, or 'next' to continue searching."
+              Zip _ x _ -> startWork x Open
+
+    Review -> io $ putStrLn $ "Currently in review mode. Complete the review using 'status', 'readd' and 'dismiss'."
+    
+
+-- the user has completely finished working on a task
+doneCmd :: Command
+doneCmd = do
+  s <- get
+  case mode s of
+    Work r m -> do io $ putStrLn $ "Marked task '" ++ r ++ "' as done.\nThe task has been removed from the list."
+                   next
+    Review -> io $ putStrLn $ "Currently in review mode. Complete the review using 'status', 'readd' and 'dismiss'."
+    Closed pass acted -> handleDone closed (\s x -> s { closed = x })
+    Open -> handleDone open (\s x -> s { open = x })
+ where handleDone access update = do 
+         s <- get
+         io $ putStrLn $ "You are not currently working on a task."
+         case access s of
+           Empty      -> next
+           c@(Zip l x r) -> do io $ putStrLn $ "You were considering: '" ++ x ++ "'"
+                               resp <- prompt "Do you want to mark it as done? (y/N) "
+                               if resp
+                                   then case r of
+                                          [] -> case l of
+                                                  [] -> do update s Empty
+                                                           next
+                                                  (y:l') -> do update s (Zip l' y r)
+                                                               next
+                                          (y:r') -> do update s (Zip l y r')
+                                                       io $ putStrLn $ "Active entry now '" ++ y ++ "'"
+                                    else return ()
+
+
+-- the user has finished working on a task for now
+stopCmd :: Command
+stopCmd = do
+  s <- get
+  case mode s of
+    Work r m -> do io $ putStrLn $ "Stopped working on '" ++ r ++ "'.\nThe task has been added to the end of the open list."
+                   next
+    Review -> io $ putStrLn $ "Currently in review mode. Complete the review using 'status', 'readd' and 'dismiss'."
+    Closed pass acted -> handleDone closed (\s x -> s { closed = x })
+    Open -> handleDone open (\s x -> s { open = x })
+ where handleDone access update = do 
+         s <- get
+         io $ putStrLn $ "You are not currently working on a task."
+         case access s of
+           Empty      -> next
+           c@(Zip l x r) -> do io $ putStrLn $ "You were considering: '" ++ x ++ "'"
+                               resp <- prompt "Do you want to stop working on it? (y/N) "
+                               if resp
+                                   then case r of
+                                          [] -> case l of
+                                                  [] -> do update s Empty
+                                                           next
+                                                  (y:l') -> do update s (Zip l' y r)
+                                                               next
+                                          (y:r') -> do update s (Zip l y r')
+                                                       io $ putStrLn $ "Active entry now '" ++ y ++ "'"
+                                    else return ()
+
+
+
+nextCmd :: Command
+nextCmd = next' True -- from the user
+
+
+-- internal next
 next :: Command
-next = do
+next = next' False -- internal, not from the user
+
+-- the actual implementation of next
+next':: Bool -> Command
+next' fromUser = do
   s <- get
   case mode s of
     Open -> case open s of
               Empty      -> do set $ s { mode = Closed 0 0 } -- not sure if this one can happen, but hey
-                               putStrLn "Open list empty, moving to closed list."
+                               io $ putStrLn "Open list empty, moving to closed list."
               Zip l x [] -> do let (x':r') = reverse (x:l)
                                set $ s { mode = Closed 0 0, open = Zip [] x' r' }
-                               putStrLn "Moving to closed list."
-                               putStrLn $ "Active entry now: '" ++ zipHead (closed s) ++ "'"
+                               io $ putStrLn "Moving to closed list."
+                               io $ putStrLn $ "Active entry now: '" ++ zipHead (closed s) ++ "'"
               Zip l x (r:rs) -> do set $ s { open = Zip (x:l) r rs }
-                                   putStrLn $ "Active entry now: '" ++ r ++ "'"
+                                   io $ putStrLn $ "Active entry now: '" ++ r ++ "'"
     Closed pass acted -> case (closed s, review s) of
                            (Empty, Zip _ _ _) -> do set $ s { mode = Review }
-                                                    putStrLn "Reached end of closed list. Moving to review."
+                                                    io $ putStrLn "Reached end of closed list. Moving to review."
+                           (Empty, Empty)     -> do set $ s { mode = Open}
+                                                    io $ putStrLn "Reached end of closed list. Moving to open list."
                            (c@(Zip _ _ []), Zip _ _ _) -> do set $ s { mode = Review, closed = flipZip c }
-                                                             putStrLn "Reached end of closed list. Moving to review."
+                                                             io $ putStrLn "Reached end of closed list. Moving to review."
                            (c@(Zip _ _ []), Empty) | acted > 0 -> do set $ s { mode = Closed (pass+1) 0, closed = flipZip c }
-                                                                     putStrLn $ "Reached end of closed list. Actioned " ++ show acted ++ " items last pass." ++
+                                                                     io $ putStrLn $ "Reached end of closed list. Actioned " ++ show acted ++ " items last pass." ++
                                                                        ++ " Returning to start of closed list."
                                                    | pass  > 0 -> do set $ s { mode = Open, closed = flipZip c }
-                                                                     putStrLn "Reached end of closed list. Did not action anything, but not first pass. Moving to open list."
+                                                                     io $ putStrLn "Reached end of closed list. Did not action anything, but not first pass. Moving to open list."
+                                                                     io $ putStrLn $ "Active entry now: '" ++ zipHead (open s) ++ "'"
                                                    | otherwise -> do set $ s { mode = Closed 0 0, review = closed s, closed = open s, open = Empty }
-                                                                     putStrLn $ "Reached end of closed list. First pass, did not action anything. " 
+                                                                     io $ putStrLn $ "Reached end of closed list. First pass, did not action anything. " 
                                                                        ++ "Closed list up for review, open list now closed. Moving to the newly closed list."
+                                                                     io $ putStrLn $ "Active entry now: '" ++ zipHead (open s) -- actually closed now
+                           (Zip l x (y:r), _) -> do set $ s { closed = Zip (x:l) y r }
+                                                    io $ putStrLn $ "Active entry now: '" ++ y ++ "'"
+    Review | fromUser  -> io $ putStrLn "'next' not allowed in Review mode. Use 'status', 'readd', 'dismiss' to complete the review first."
+           | otherwise -> case review s of
+                            Empty         -> endReview
+                            Zip _ _ []    -> endReview
+                            Zip l x (y:r) -> do set $ s { review = Zip (x:l) y r }
+                                                io $ putStrLn $ "Active entry now: '" ++ y ++ "'"
+                          where endReview = do set $ s { mode = Closed 0 0 }
+                            io $ putStrLn $ "Reached end of review. Moving to closed list."
+                            io $ putStrLn $ "Active entry now: '" ++ zipHead (closed s) ++ "'"
 
+    Work r m -> do io $ putStrLn $ "Currently working on '" ++ r ++ "'."
+                   io $ putStrLn $ "You must either 'stop' working or mark this task 'done'."
 
+    
 
 -- state machine. if at the end of:
 -- open -- go to closed
